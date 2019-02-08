eeb40b4f35b9cc19bad23824cd8eab06b00da6f9
Richard Hill III <rhillz718@gmail.com>
2019-02-08 07:02:48 -0500

    initial commit

---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem svwqwf qlkjf lkjqbfkq     lknqfn qlbnflqb lfjbclqnjbwv hwaqhv lufwujf lubdf;lsa',
+            'author': 'Rhillx',
+            'create_date': '02-07-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem svwqwf qlkjf lkjqbfkq     lknqfn qlbnflqb lfjbclqnjbwv hwaqhv lufwujf lubdf;lsa',
+            'author': 'Rhillx',
+            'create_date': '02-07-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem svwqwf qlkjf lkjqbfkq     lknqfn qlbnflqb lfjbclqnjbwv hwaqhv lufwujf lubdf;lsa',
+            'author': 'Rhillx',
+            'create_date': '02-07-2019'
+        },
+        {
+            'id': 4,
+            'title': 'Article Four',
+            'body': 'Lorem svwqwf qlkjf lkjqbfkq     lknqfn qlbnflqb lfjbclqnjbwv hwaqhv lufwujf lubdf;lsa',
+            'author': 'Rhillx',
+            'create_date': '02-07-2019'
+        },
+    ]
+    return articles
